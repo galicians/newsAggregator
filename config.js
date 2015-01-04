214d880e6748ee3c73cb1bb95890ecf7bea332ab
galicians <galicians@gmail.com>
2015-01-04 21:43:40 +0000

    config removed from gitignore

---
+++ config.js
@@ -0,0 +1,16 @@
+var bbcSource = "http://feeds.bbci.co.uk/news/rss.xml";
+var skySource = "http://feeds.skynews.com/feeds/rss/uk";
+var hackerSource = "http://feeds.feedburner.com/TheHackersNews";
+exports.port = process.env.PORT || 3000;
+exports.sources = [];
+exports.frecuency = 10000;
+exports.dbUrl = process.env.DATABASE_URL || 'mongodb://localhost/aggregatornews';
+exports.queryLimit = 2000;
+
+exports.sources.push(bbcSource);
+exports.sources.push(skySource);
+exports.sources.push(hackerSource);
+
+
+
+
