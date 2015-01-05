70de4fdb2c02a25ccd11f48baf68e0da90923251
galicians <galicians@gmail.com>
2015-01-05 12:30:02 +0000

    heroku

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var hackerSource = "http://feeds.feedburner.com/TheHackersNews";
 exports.port = process.env.PORT || 3000;
 exports.sources = [];
 exports.frecuency = 10000;
-exports.dbUrl = process.env.DATABASE_URL || 'mongodb://localhost/aggregatornews';
+exports.dbUrl = MONGOLAB_URI || 'mongodb://localhost/aggregatornews';
 exports.queryLimit = 2000;
 
 exports.sources.push(bbcSource);
